864b52905222053f22b0f218ea810bd2d92c109a
Pablo Orduña <pablo@labsland.com>
2024-01-07 17:35:16 -0800

    Add comments to the scheduler

--- reliascheduler/views/scheduler.py
+++ reliascheduler/views/scheduler.py
@@ -5,15 +5,10 @@ import secrets
 import logging
 from datetime import datetime
 
-from typing import Optional
-
-from urllib.request import urlopen
-
 import yaml
 import numpy as np
 
-from werkzeug.utils import secure_filename
-from flask import Blueprint, jsonify, current_app, request, make_response, send_file
+from flask import Blueprint, jsonify, current_app, request
 
 from reliascheduler import redis_store
 from reliascheduler.auth import check_backend_credentials, check_device_credentials
@@ -142,8 +137,12 @@ def poll(task_id):
     return jsonify(success=True)
 
 @scheduler_blueprint.route('/user/tasks/', methods=['POST'])
-def load_task():
-
+def create_task():
+    """
+    This method is called by the backend, and the purpose is to create a new task.
+    Typically, the user calls the backend every time they want to submit some code, and then
+    the backend calls the scheduler using this method.
+    """
     authenticated = check_backend_credentials()
     if not authenticated:
         return jsonify(success=False, taskIdentifier=None, status=None, message="Invalid secret"), 401
@@ -457,6 +456,10 @@ def _stop_task_if_inactive(device_base: str, task_id: str) -> bool:
 
 @scheduler_blueprint.route('/devices/tasks/receiver')
 def assign_task_primary():
+    """
+    Assign a task to the receiver. The receiver is the primary device: only once the receiver has received a taks, the transmitter
+    gets the task.
+    """
     device = check_device_credentials()
     if device is None:
         return jsonify(success=False, grcFile=None, grcFileContent=None, taskIdentifier=None, sessionIdentifier=None, message="Invalid device credentials"), 401
@@ -527,6 +530,10 @@ def assign_task_primary():
 
 @scheduler_blueprint.route('/devices/tasks/transmitter')
 def assign_task_secondary():
+    """
+    Assign a task to the transmitter. The transmitter is the secondary device: it waits until the receiver
+    is assigned a task to be assigned a task.
+    """
     device = check_device_credentials()
     if device is None:
         return jsonify(success=False, grcFile=None, grcFileContent=None, taskIdentifier=None, sessionIdentifier=None, message="Invalid device credentials"), 401
@@ -591,10 +598,3 @@ def assign_error_message(task_identifier):
     redis_store.hset(t, TaskKeys.errorMessage, request_data.get('errorMessage'))
     redis_store.hset(t, TaskKeys.errorTime, request_data.get('errorTime'))
     return jsonify(success=True, message="Success")
-
-def _corsify_actual_response(response):
-    response.headers['Access-Control-Allow-Origin'] = '*';
-    response.headers['Access-Control-Allow-Credentials'] = 'true';
-    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS, GET, POST';
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control';
-    return response
